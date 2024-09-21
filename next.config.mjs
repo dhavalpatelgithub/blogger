0a9beccd0975d53b3569e31eb1aa69df3bdf09c1
Dhaval Patel <pateldhaval891@gmail.com>
2024-09-21 18:07:38 +0530

    navbar + sample blog page create + og image testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'flowbite.com',
+            port: '',
+            pathname: '/docs/images/people/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'flowbite.s3.amazonaws.com',
+            port: '',
+            pathname: '/**',
+          },
+        ],
+      }
+};
 
 export default nextConfig;
