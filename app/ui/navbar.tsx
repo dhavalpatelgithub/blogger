e9ce288e6dcabde0495c386e95b74bfdabc0ba3c
Dhaval Patel <pateldhaval891@gmail.com>
2024-09-28 14:44:49 +0530

    image replacement

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -80,7 +80,6 @@ export default function Navbar() {
         </>
     }
 
-    debugger;
     if (blogId) {
         langButtonWithMenu = <><button id="dropdownDefaultButton" onClick={showLanguageMenu} data-dropdown-toggle="langDropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
             Language
