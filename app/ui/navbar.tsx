1c59a36d8c4ead098431075451dc79e7aec83279
Dhaval Patel <pateldhaval891@gmail.com>
2024-10-01 20:22:45 +0530

    Mobile UI issue fixed

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -35,12 +35,22 @@ export default function Navbar() {
         label: 'Chinese'
     }]
 
-    const showMobileMenu = () => {
+    const toggleMobileMenu = () => {
+        hideLanguageMenu();
         setIsMobileMenuVisible(!isMobileMenuVisible);
     }
 
-    const showLanguageMenu = ()  => {
+    const hideMobileMenu = () => {
+        setIsMobileMenuVisible(false);
+    }
+
+    const toggleLanguageMenu = ()  => {
+        hideMobileMenu();
         setIsLangMenuVisible(!isLangMenuVisible);
+    }
+
+    const hideLanguageMenu = ()  => {
+        setIsLangMenuVisible(false);
     }
 
     let lang = params.lang;
@@ -50,7 +60,7 @@ export default function Navbar() {
     }
 
     const selectLanguage = (language: string) => {
-         showLanguageMenu();
+         toggleLanguageMenu();
          LangStorage.setLang(language);
          lang = language;
     }
@@ -99,7 +109,7 @@ export default function Navbar() {
     }
 
     if (blogId || pathname.includes('/blog')) {
-        langButtonWithMenu = <><button id="dropdownDefaultButton" onClick={showLanguageMenu} data-dropdown-toggle="langDropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+        langButtonWithMenu = <><button id="dropdownDefaultButton" onClick={toggleLanguageMenu} data-dropdown-toggle="langDropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
             Language
             <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
@@ -107,7 +117,7 @@ export default function Navbar() {
         </button>
         { langMenu }</>
     } else {
-        langButtonWithMenu = <button id="dropdownDefaultButton" onClick={showLanguageMenu} data-dropdown-toggle="langDropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+        langButtonWithMenu = <button id="dropdownDefaultButton" data-dropdown-toggle="langDropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
             <Link href={`/${lang}/blog`}>Blogs</Link>
         </button>
     }
@@ -128,11 +138,11 @@ export default function Navbar() {
                 <div className="flex md:order-2 md:space-x-0 rtl:space-x-reverse">
                     <ThemeToggle />
                     {langButtonWithMenu}
-                    <button data-collapse-toggle="navbar-sticky" type="button" onClick={showMobileMenu} className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                    <button data-collapse-toggle="navbar-sticky" type="button" onClick={toggleMobileMenu} className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                         {menuToggleSvg}
                     </button>
                 </div>
-                <div onClick={showMobileMenu} className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isMobileMenuVisible ? '' : 'hidden'}`} id="navbar-sticky">
+                <div onClick={toggleMobileMenu} className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isMobileMenuVisible ? '' : 'hidden'}`} id="navbar-sticky">
                     <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li key="home">
                             <Link href="/" className={`${pathname === "/" ? 'text-blue-700 dark:text-blue-500' : 'text-gray-900 dark:text-white'} block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}>Home</Link>
